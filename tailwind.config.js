72514ecb600747b0558f969056961c9efd205ecd
Mahdi <48333830+mahdi-mk@users.noreply.github.com>
2022-12-17 00:24:09 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+const { colors: defaultColors } = require('tailwindcss/defaultTheme')
+
+const colors = {
+  ...defaultColors,
+  ...{
+    'primary': '#171628',
+    'custom-blue': '#1abee9',
+    'custom-pink': '#f15369',
+  },
+}
+
+module.exports = {
+  content: ['./views/**/*.html'],
+  theme: {
+    // colors: colors,
+    extend: {
+      colors
+    },
+  },
+  plugins: [],
+}
